47b1698157004c95c12a105f25bd3c7e5a6bc61b
claire <1097457668@qq.com>
2021-11-09 15:06:09 +0100

    Test ayoub

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def hello_name(name):
     name= input()
-    if name == "Thanh Lan":
-        print("Hello Thanh Lan!")
+    if name == "Thanh Lan" or name == "claire":
+        print("Hello Thanh Lan ou claire!")
     else:
         print("Je ne vous connais pas.")
         
